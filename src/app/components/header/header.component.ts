5c6e7603066e120ee401171611901becd7b38ddf
Parker <parkersam@gmail.com>
2025-02-24 21:01:54 -0700

    Add(header) because of repeated use

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, input, OnInit } from '@angular/core';
+import { IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle],
+})
+export class HeaderComponent implements OnInit {
+  title = input('title');
+
+  constructor() {}
+
+  ngOnInit() {}
+}
