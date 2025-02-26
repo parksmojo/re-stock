267728e2a1adda998bc741b652ff02b5407e3bac
Parker <parkersam@gmail.com>
2025-02-26 16:38:51 -0700

    Add(item-list)with basic functionality

---
+++ src/app/components/item-list/item-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from 'src/app/model/item/item';
+
+@Component({
+  selector: 'app-item-list',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.scss'],
+})
+export class ItemListComponent implements OnInit {
+  @Input() items: Item[] = [];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
