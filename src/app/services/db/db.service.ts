58d6ebc3c34e867e438e8dc17182a71540ca3025
Parker <parkersam@gmail.com>
2025-02-27 21:24:05 -0700

    Implement(db)

--- src/app/services/db/db.service.ts
+++ src/app/services/db/db.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { FakeData } from 'src/app/model/fake-data/fake-data';
+import { Pantry } from 'src/app/model/pantry/pantry';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DbService {
-  constructor() {}
+  private _pantry: Pantry;
+
+  constructor() {
+    this._pantry = Pantry.fromData(FakeData.pantry);
+  }
+
+  public updatePantry() {
+    this._pantry = Pantry.fromData(FakeData.pantry);
+  }
+
+  get pantry() {
+    return this._pantry;
+  }
 }
